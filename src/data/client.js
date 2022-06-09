651dc97cb858867a92a8b0589917458736a5ae34
Gavin S <stahlgavin@gmail.com>
2022-06-09 14:34:30 -0700

    client connected, login added

---
+++ src/data/client.js
@@ -0,0 +1,2 @@
+export const DOMAIN = "dev-vmhaa30k.us.auth0.com"
+export const CLIENT_ID = "a2L7IsLRpXL0Xazk39YL8dKmtGievtlp"
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import {DOMAIN, CLIENT_ID} from "./data/client"
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
