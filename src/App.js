5f7f6fac65f4b48ade092babb298ac3325ba6d41
Gavin S <stahlgavin@gmail.com>
2022-06-09 14:42:02 -0700

    logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
-import {Login} from "./components/Login"
+import Login from "./components/Login"
+import Logout from "./components/Logout"
 
 function App() {
   return (
     <div>
       <Login />
+      <Logout />
     </div>
   )
 }
